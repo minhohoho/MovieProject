48c60e0819b1e27dcf1e6c66b7087699f0b7d180
seokminho <samm6754@gmail.com>
2024-02-02 15:27:58 +0900

    #7-[영화 전체 검색]-레파지토리

--- src/main/java/com/example/movieproject/repository/MovieRepository.java
+++ src/main/java/com/example/movieproject/repository/MovieRepository.java
@@ -1,6 +1,8 @@
 package com.example.movieproject.repository;
 
 import com.example.movieproject.domain.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
