d062dc371c881f6e8d1518f4402ada23f22e8574
seokminho <samm6754@gmail.com>
2024-02-09 15:25:29 +0900

    #11-[oauth2]-서비스,레파지토리

---
+++ src/main/java/com/example/movieproject/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.movieproject.repository;
+
+import com.example.movieproject.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
