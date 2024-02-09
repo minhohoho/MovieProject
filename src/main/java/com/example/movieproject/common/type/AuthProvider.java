023255a7a4948f9a5ab28e9e708150402ba24092
seokminho <samm6754@gmail.com>
2024-02-09 15:24:15 +0900

    #11-[oauth2]-도메인

---
+++ src/main/java/com/example/movieproject/common/type/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.example.movieproject.common.type;
+
+public enum AuthProvider {
+    GOOGLE,
+    NAVER,
+    KAKAO
+}
---
+++ src/main/java/com/example/movieproject/common/type/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.movieproject.common.type;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
