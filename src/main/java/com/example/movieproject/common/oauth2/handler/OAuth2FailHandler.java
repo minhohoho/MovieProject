87cba02705d89976d7b5eb068b836426c230d119
seokminho <samm6754@gmail.com>
2024-02-09 15:23:36 +0900

    #11-[oauth2]-Handler
    oaut2 로그인 성공 실패 분기를 나누어주는 클래스이다
    만약 성공한다면 사용자 정보를 이용해 jwt토큰을 생성할 예정이다

---
+++ src/main/java/com/example/movieproject/common/oauth2/handler/OAuth2FailHandler.java
@@ -0,0 +1,22 @@
+package com.example.movieproject.common.oauth2.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2FailHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.sendRedirect("http://localhost:8080/");
+    }
+}
