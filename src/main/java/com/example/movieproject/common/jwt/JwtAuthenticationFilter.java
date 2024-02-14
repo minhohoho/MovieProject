45a9cb021b92492ea1fb06de43a6f1ab51278f43
seokminho <samm6754@gmail.com>
2024-02-14 14:01:34 +0900

    #12-[jwt]-jwt
    jwt 토큰을 생성하고, 필터에서 복호화 하고,  에러에 대응 하는 클래스를 만들었다.

---
+++ src/main/java/com/example/movieproject/common/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.example.movieproject.common.jwt;
+
+import com.example.movieproject.common.oauth2.info.UserPrincipal;
+import io.jsonwebtoken.IncorrectClaimException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String accessToken = resolveToken(request);
+        log.info("filter 통과");
+
+        try {
+            if(accessToken != null && jwtProvider.validateToken(accessToken)) {
+                Authentication authentication = jwtProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("security 객체 저장 완료");
+            }
+        } catch (IncorrectClaimException e) {
+            SecurityContextHolder.clearContext();
+            log.debug("Invalid JWT token.");
+            response.sendError(403);
+        } catch (UsernameNotFoundException e){
+            SecurityContextHolder.clearContext();
+            log.debug("Can't find user.");
+            response.sendError(403);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    public String resolveToken(HttpServletRequest httpServletRequest) {
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/example/movieproject/common/jwt/JwtProvider.java
+++ src/main/java/com/example/movieproject/common/jwt/JwtProvider.java
@@ -1,16 +1,144 @@
 package com.example.movieproject.common.jwt;
 
-import com.example.movieproject.dto.response.MemberResponseDTO;
+import com.example.movieproject.common.oauth2.info.UserPrincipal;
+import com.example.movieproject.common.type.UserRole;
+import com.example.movieproject.dto.response.TokenResponseDTO;
+import com.example.movieproject.repository.RedisTokenRepository;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-@Component
-public class JwtProvider {
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public MemberResponseDTO generateAccessToken(){
-        return MemberResponseDTO.builder()
-                .accessToken("임시 jwt 토큰")
+@Component
+@Slf4j
+public class JwtProvider implements InitializingBean {
+
+        private static final String AUTHORITIES_KEY="ROLE";
+        private static final String MEMBER_ID_KEY ="MEMBER_ID";
+
+        private final String secretKey;
+        private static Key signingKey;
+        private final Long  accessTokenValidityInMilliseconds;
+        private final Long refreshTokenValidityInMilliseconds;
+
+        private final RedisTokenRepository redisTokenRepository;
+
+        public JwtProvider(
+                @Value("${jwt.secret}") String secretKey,
+                @Value("${jwt.access-token-validity-in-seconds}") Long accessTokenValidityInMilliseconds,
+                @Value("${jwt.refresh-token-validity-in-seconds}") Long refreshTokenValidityInMilliseconds,
+                RedisTokenRepository redisTokenRepository
+        ){
+            this.secretKey=secretKey;
+            this.accessTokenValidityInMilliseconds=accessTokenValidityInMilliseconds;
+            this.refreshTokenValidityInMilliseconds=refreshTokenValidityInMilliseconds;
+            this.redisTokenRepository=redisTokenRepository;
+        }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] secretKeyBytes = Decoders.BASE64.decode(secretKey);
+        signingKey = Keys.hmacShaKeyFor(secretKeyBytes);
+    }
+
+    public TokenResponseDTO createToken(Long memberId, UserRole role){
+        Long now = System.currentTimeMillis();
+
+            String accessToken = Jwts.builder()
+                    .setHeaderParam("typ","JWT")
+                    .setHeaderParam("alg","HS512")
+                    .setExpiration(new Date(now+accessTokenValidityInMilliseconds))
+                    .setSubject("access-token")
+                    .claim(MEMBER_ID_KEY,memberId)
+                    .claim(AUTHORITIES_KEY,role)
+                    .signWith(signingKey,SignatureAlgorithm.HS512)
+                    .compact();
+
+        String refreshToken = Jwts.builder()
+                .setHeaderParam("typ","JWT")
+                .setHeaderParam("alg","HS512")
+                .setExpiration(new Date(now+refreshTokenValidityInMilliseconds))
+                .setSubject("refresh-token")
+                .claim(MEMBER_ID_KEY,memberId)
+                .signWith(signingKey,SignatureAlgorithm.HS512)
+                .compact();
+        redisTokenRepository.save(memberId,refreshToken,refreshTokenValidityInMilliseconds);
+
+
+
+        return TokenResponseDTO.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
                 .build();
     }
 
+       public Authentication getAuthentication(String token){
+           Long memberId = Long.parseLong(getClaims(token).get(MEMBER_ID_KEY).toString());
+           UserRole role = UserRole.valueOf(getClaims(token).get(AUTHORITIES_KEY).toString());
+
+           UserPrincipal userPrincipal = UserPrincipal.save(memberId,role);
+
+           return new UsernamePasswordAuthenticationToken(userPrincipal,"",userPrincipal.getAuthorities());
+       }
+
+       public Claims getClaims(String token){
+            try{
+                return Jwts.parserBuilder()
+                        .setSigningKey(signingKey)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+            }catch (ExpiredJwtException e){
+                return e.getClaims();
+            }
+       }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(signingKey).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+    public Boolean reissueToken(String refreshToken){
+        Long now = System.currentTimeMillis();
+        Long memberId = Long.parseLong(getClaims(refreshToken).get(MEMBER_ID_KEY).toString());
+
+        if(redisTokenRepository.isExists(memberId) && validateToken(refreshToken)){
+            String reissueToken = Jwts.builder()
+                    .setHeaderParam("typ","JWT")
+                    .setHeaderParam("alg","HS512")
+                    .setExpiration(new Date(now+refreshTokenValidityInMilliseconds))
+                    .setSubject("refresh-token")
+                    .claim(MEMBER_ID_KEY,memberId)
+                    .signWith(signingKey,SignatureAlgorithm.HS512)
+                    .compact();
+            redisTokenRepository.save(memberId,reissueToken,refreshTokenValidityInMilliseconds);
+            return  true;
+        }
+            return  false;
+    }
 
 }
