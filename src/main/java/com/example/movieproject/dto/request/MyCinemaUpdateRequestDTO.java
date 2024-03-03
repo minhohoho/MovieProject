ee88af34ef6538ea46c008e0ebf2dfb7299393ec
seokminho <samm6754@gmail.com>
2024-03-03 15:47:49 +0900

    #17-[내 영화관]-업데이트 기능 추가

---
+++ src/main/java/com/example/movieproject/dto/request/MyCinemaUpdateRequestDTO.java
@@ -0,0 +1,21 @@
+package com.example.movieproject.dto.request;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class MyCinemaUpdateRequestDTO {
+
+    private String cinemaName;
+
+    private String cinemaDetail;
+
+    private String cinemaItem;
+
+    private String addressName;
+
+
+
+}
